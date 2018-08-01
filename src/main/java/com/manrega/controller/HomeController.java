3561368aa033ee8c60bebda6488a42f2463a9997
Abhiram Hatolkar <ahatolkar@grepruby.com>
2018-08-01 19:56:06 +0530

    setup of java app

---
+++ src/main/java/com/manrega/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.manrega.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello! This is Manrega app!";
+    }
+
+}
