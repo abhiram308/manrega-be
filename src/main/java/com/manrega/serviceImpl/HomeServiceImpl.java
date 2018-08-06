db9002ebaa2683f659ee694775377494e90c8701
Abhiram Hatolkar <ahatolkar@grepruby.com>
2018-08-06 21:05:54 +0530

    Updated home service to show text and links

--- src/main/java/com/manrega/serviceImpl/HomeServiceImpl.java
+++ src/main/java/com/manrega/serviceImpl/HomeServiceImpl.java
@@ -2,21 +2,26 @@ package com.manrega.serviceImpl;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.springframework.stereotype.Component;
 
 import com.manrega.service.HomeService;
 
 @Component
-public class HomeServiceImpl implements HomeService{
+public class HomeServiceImpl implements HomeService {
 
 	public String getData() {
-		String data;
+		StringBuilder data = new StringBuilder();
 		try {
-			Document doc = Jsoup.connect("http://www.baeldung.com/java-with-jsoup").get();
-			data = doc.toString();
-    		} catch (Exception ex) {
-    			data = "something went wrong";
-    		}
-		return data;	
+			Document doc = Jsoup.connect("https://www.ibm.com/developerworks/cloud/library/cl-add-an-organization-to-your-hyperledger-fabric-blockchain/index.html").get();
+
+			for (Element element : doc.select("a")) {
+				data = data.append("<p>" + element.text() + "</p><b>" + element.attr("href") + "</b></br>");
+			}
+
+		} catch (Exception ex) {
+			data.append("Exception occured");
+		}
+		return data.toString();
 	}
 }
